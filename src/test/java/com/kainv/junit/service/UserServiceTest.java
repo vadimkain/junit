70867444ebd5d68e3b96f931b676061602797e0e
vadimkain <48841785+vadimkain@users.noreply.github.com>
2023-02-12 23:19:00 +0200

    JUnit 5. 2. Установка программного обеспечения

---
+++ src/test/java/com/kainv/junit/service/UserServiceTest.java
@@ -0,0 +1,12 @@
+package com.kainv.junit.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserServiceTest {
+    @Test
+    void test() {
+        assertTrue(false);
+    }
+}
