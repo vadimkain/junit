df5bf54488d72b1deae4e43f409a1dcb1906249d
vadimkain <48841785+vadimkain@users.noreply.github.com>
2023-02-14 18:49:17 +0200

    JUnit 5. 5. Запуск тестов. Launcher API

---
+++ src/test/java/com/kainv/junit/TestLauncher.java
@@ -0,0 +1,35 @@
+package com.kainv.junit;
+
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+
+import java.io.PrintWriter;
+
+public class TestLauncher {
+    public static void main(String[] args) {
+        Launcher launcher = LauncherFactory.create();
+
+//        launcher.registerLauncherDiscoveryListeners();
+//        launcher.registerTestExecutionListeners();
+
+        SummaryGeneratingListener summaryGeneratingListener = new SummaryGeneratingListener();
+
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder
+                .request()
+//                Указываем, где наш request будет искать наши тесты
+//                .selectors(DiscoverySelectors.selectClass(UserServiceTest.class))
+                .selectors(DiscoverySelectors.selectPackage("com.kainv.junit.service"))
+                .build();
+
+        launcher.execute(request, summaryGeneratingListener);
+
+//        Выводим результат тестирования
+        try (PrintWriter writer = new PrintWriter(System.out)) {
+            summaryGeneratingListener.getSummary().printTo(writer);
+        }
+    }
+}
--- src/test/java/com/kainv/junit/service/UserServiceTest.java
+++ src/test/java/com/kainv/junit/service/UserServiceTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class UserServiceTest {
+public class UserServiceTest {
 
     private UserService userService;
 
